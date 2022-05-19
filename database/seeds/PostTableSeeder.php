82ceeaa7e2a22222192b972be765c0e7bff5339e
Alenfer89 <alberioalex@gmail.com>
2022-05-19 19:13:12 +0200

    add posts to db with migration and seed

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -17,11 +17,11 @@ class PostTableSeeder extends Seeder
         for ($i=0; $i < 50 ; $i++) { 
             $addPost = new Post();
             $random = rand(1 , 150);
-            $addPost->title = ucfirst($faker->unique()->words(rand(1 , 5)), true);
+            $addPost->title = ucfirst($faker->unique()->words(rand(1 , 5), true));
             $addPost->author_name = $faker->name();
             $addPost->content = $faker->paragraphs(rand(1 , 3), true);
             $addPost->image_url = "https://picsum.photos/id/$random/350/500";
-            $addPost->slug = Str::slug($addPost->title, "-") ."-". $i + 1;
+            $addPost->slug = Str::slug($addPost->title, "-") ."-". ($i + 1);
             $addPost->save();
             
         }
