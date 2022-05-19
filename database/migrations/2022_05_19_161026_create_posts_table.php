5221b5160326ac7e3ab3cba2bad0aac51dbb9112
Alenfer89 <alberioalex@gmail.com>
2022-05-19 18:40:40 +0200

    popuilate post migration

--- database/migrations/2022_05_19_161026_create_posts_table.php
+++ database/migrations/2022_05_19_161026_create_posts_table.php
@@ -15,6 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('author_name', 50);
+            $table->text('content');
+            $table->text('image_url')->nullable();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
