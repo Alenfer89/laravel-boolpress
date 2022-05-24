42ad2e3ec939a37bae33c3e2d13af91f212a83ec
Alenfer89 <alberioalex@gmail.com>
2022-05-24 15:34:45 +0200

    add style and links guest home page

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,13 +4,38 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-6 mx-auto">
+        <div class="col-12 mx-auto">
             <h1>
                 GUEST (NOT LOGGED) HOMEPAGE
             </h1>
         </div>
-        <div class="col-6 mx-auto">
-            <a href="{{ url('/') }}"> back</a>
+        <section class="col-12">
+            <h1>
+                some news
+            </h1>
+            <h1>
+                some news
+            </h1>
+            <h1>
+                some news
+            </h1>
+        </section>
+        <div class="col-6 mx-auto d-flex justify-content-between">
+            <a href="{{ url('/') }}">
+                <button class="btn btn-primary" >
+                    back
+                </button> 
+            </a>
+            <div>
+                <a class="nav-link" href="{{ route('login') }}">
+                    {{ __('Login') }}
+                </a>
+                @if (Route::has('register'))
+                    <a class="nav-link" href="{{ route('register') }}">
+                        {{ __('Register') }}
+                    </a>
+                @endif
+            </div>
         </div>
     </div>
 </div>
