28649e49003b5623be841f48cf6ffb4429505950
Alenfer89 <alberioalex@gmail.com>
2022-05-24 15:38:53 +0200

    add style admin home page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,14 +4,33 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-6 mx-auto">
+        <div class="col-12 text-center">
             <h1>
                 LOGGED HOMEPAGE
             </h1>
         </div>
-        <div class="col-6 mx-auto">
-            <a href="{{ route('admin.posts.index') }}"> i tuoi post </a>
+        <section class="col-12">
+            <h1>
+                some content
+            </h1>
+        </section>
+        <section class="col-12">
+            <h1>
+                some content
+            </h1>
+        </section>
+        <div class="col-12">
+            <h1>
+                <a href="{{ route('admin.posts.index') }}">
+                    POSTS
+                </a>
+            </h1>
         </div>
+        <section class="col-12">
+            <h1>
+                CATEGORIES
+            </h1>
+        </section>
     </div>
 </div>
 @endsection
