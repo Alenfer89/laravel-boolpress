373f39613c8667339c76d9d1a4a9341d012adb44
Alenfer89 <alberioalex@gmail.com>
2022-05-26 15:49:21 +0200

    expose api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('posts')->group(function(){
+    Route::get('/', 'Api\PostController@index');
+    Route::get('/{post}', 'Api\PostController@show');
+});
