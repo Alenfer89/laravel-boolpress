8c693922031dae3815be0617bf0d7ab64198103d
Alenfer89 <alberioalex@gmail.com>
2022-05-26 17:35:30 +0200

    add card to show elements

--- resources/js/components/Post.vue
+++ resources/js/components/Post.vue
@@ -1,17 +1,40 @@
 <template>
-    <div>
-        <h6>
-            test
-        </h6>
+    <div class="col-4">
+        <div class="card mb-4">
+            <img :src="post.image_url" class="card-img-top" :alt="`Image of post` + post.title">
+            <div class="card-body">
+                <h5 class="card-title">
+                    {{ post.title }}
+                </h5>
+                <p class="class-subtitle">
+                    by: {{ post.user_id }}
+                </p>
+                <p class="card-text">
+                    {{ post.content.substring(0, 100) + ".." }}
+                </p>
+                <a href="" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'Post',
+    components:{
 
+    },
+    props: ['post']
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+div.card{
+    //height: 80vh;
+    //overflow: scroll;
+    p.card-text{
+        max-height: 4rem;
+        font-size: 0.7rem;
+    }
+}
 </style>
--- resources/js/components/PostList.vue
+++ resources/js/components/PostList.vue
@@ -1,18 +1,45 @@
 <template>
-    <div>
-        <h1>test</h1>
-        <Post />
+    <div class="row justify-content-between align-items-center">
+        
+        <Post 
+        v-for="(post, index) in posts"
+        :key="index"
+        :post='post'
+        />
+
     </div>
 </template>
 
 <script>
+import Axios from 'axios';
 import Post from './Post.vue';
 export default {
     name: "PostList",
-    data: function () {
-    },
     components: { 
         Post,
+    },
+    data: function () {
+        return{
+            posts: []
+        }
+    },
+    methods:{
+        getPosts(){
+            Axios.get('http://127.0.0.1:8000/api/posts')
+                .then((result) =>{
+                    //console.log(result.data);
+                    //console.log(result.data.results);
+                    //console.log(result.data.results.data);
+                    this.posts = result.data.results.data;
+                    console.log(this.posts);
+                })
+                .catch((error) =>{
+                    console.warn(error)
+                })
+        }
+    },
+    created(){
+        this.getPosts();
     }
 }
 </script>
